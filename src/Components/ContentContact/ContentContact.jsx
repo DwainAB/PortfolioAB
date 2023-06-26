b375db064a1eb0763a89dbbaaf44f2d9f9fe6dc2
dwain93 <74243765+dwain93@users.noreply.github.com>
2023-06-26 12:44:33 +0200

    Configuration formulaire

--- src/Components/ContentContact/ContentContact.jsx
+++ src/Components/ContentContact/ContentContact.jsx
@@ -1,39 +1,58 @@
 import React from "react";
 import "./ContentContact.css"
+import emailjs from "@emailjs/browser"
+import './ContentContact.css';
+import { useRef } from "react";
+
+
 
 function ContentContact(){
+
+        const form = useRef();
+        const sendEmail = (e) => {e.preventDefault();
+    
+        emailjs.sendForm('service_kqxcgg2', 'template_f8nbdce', form.current, 'G2Lyl3lT-i8pW2UQw')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
+
+
+  
     return(
         <div className="container-contact">
             <div className="container-contact-left">
                 <div className="card">
                     <div className="card2">
-                        <form className="form">
+                        <form className="form" ref={form} onSubmit={sendEmail}>
                             <p id="heading">Contact</p>
                             <div className="input-container field">
                                 <p className="bash-text">
                                     <span className="user">user</span><span className="vm">@name</span>:<span className="char">~</span>$
                                 </p>
-                                <input className="input" placeholder="git config --global user.name" type="text"/>
+                                <input name="name" className="input" placeholder="git config --global user.name" type="text" required/>
                             </div>
                             <div className="input-container field">
                                 <p className="bash-text">
                                     <span className="user">user</span><span className="vm">@mail</span>:<span className="char">~</span>$
                                 </p>
-                                <input className="input" placeholder="git config --global user.email" type="email"/>
+                                <input name="email" className="input" placeholder="git config --global user.email" type="email" required/>
                             </div>
                             <div className="input-container field">
                                 <p className="bash-text">
                                     <span className="user">user</span><span className="vm">@objet</span>:<span className="char">~</span>$
                                 </p>
-                                <input className="input" placeholder="git add 'Objet'" type="text"/>
+                                <input name="object" className="input" placeholder="git add 'Objet'" type="text" required/>
                             </div>
                             <div className="input-container field">
                                 <p className="bash-text input-message test">
                                     <span className="user">user</span><span className="vm">@message</span>:<span className="char">~</span>$
                                 </p>
-                                <input className="input input-message" placeholder="git commit -m 'message'" type="text"/>
+                                <input name="message" className="input input-message" placeholder="git commit -m 'message'" type="text" required/>
                             </div>
-                            <button className="button3">Git push</button>
+                            <button type="submit" className="button3">Git push</button>
                         </form>
                     </div>
                 </div>
